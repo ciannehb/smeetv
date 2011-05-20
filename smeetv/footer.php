228d87793ac987a8326f28f0802dfb515a37afc7
f1vlad <kulchitski@gmail.com>
2011-05-19 23:57:22 -0400

    Add privacy policy

--- smeetv/footer.php
+++ smeetv/footer.php
@@ -1,1 +1,1 @@
-<footer>&copy; 2011 smeetv.com <small>send feedback to smeetvcom at gmail</small></footer></body></html>
+<footer>&copy; 2011 smeetv.com <small>send feedback to smeetvcom at gmail. <a href="/privacy/">privacy policy</small></footer></body></html>
