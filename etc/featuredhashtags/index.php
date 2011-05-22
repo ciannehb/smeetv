973a3d1866679a6c46a2c3015feab53000bba97a
f1vlad <kulchitski@gmail.com>
2011-05-22 03:44:29 -0400

    Improve featured hash tag logic

--- etc/featuredhashtags/index.php
+++ etc/featuredhashtags/index.php
@@ -5,12 +5,12 @@ require_once($_SERVER["DOCUMENT_ROOT"].'/smeetv/func.php');
 connect2db();
 
 
-$qh="select keyword,counter from keywords order by counter desc";
+$qh="select keyword,counter from keywords order by counter desc limit 0,30";
 $qh=mysql_query($qh);
 for($i=0;$i<mysql_num_rows($qh);$i++){
     $gh=mysql_fetch_array($qh);
     echo "
-         <span><a href=\"\" style=\"font-size:".($gh['counter']+10)."px\" title=\"{$gh['keyword']}\" id=\"{$gh['keyword']}\">{$gh['keyword']}</a></span>
+         <span><a href=\"\" style=\"font-size:".(($gh['counter']*2)+10)."px\" title=\"{$gh['keyword']}\" id=\"{$gh['keyword']}\">{$gh['keyword']}</a></span>
     ";
 }
 
