9fed8b2d2f90c71448aa1305bb22ceca4b10fecd
f1vlad <kulchitski@gmail.com>
2011-05-30 20:21:10 -0400

    More style adjustments

--- smeetv/footer.php
+++ smeetv/footer.php
@@ -1,4 +1,4 @@
-<hr class="clear">
+<hr class="clearfix">
 <footer id="ft">
 <aside><a href="http://twitter.com/smeetv">follow us on twitter</a>. send feedback to <strong>smeetvcom at gmail</strong>. <a href="/privacy/">privacy policy</a></aside>
 &copy; 2011 smeetv.com
