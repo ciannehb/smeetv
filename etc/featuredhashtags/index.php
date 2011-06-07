3714e16186de13bf51abb509657a309ceacd0dc9
f1vlad <kulchitski@gmail.com>
2011-06-06 20:36:46 -0400

    Work on filter bad words

--- etc/featuredhashtags/index.php
+++ etc/featuredhashtags/index.php
@@ -1,18 +1,27 @@
 <? // in this script we check for new updates
 session_start();
 require_once($_SERVER["DOCUMENT_ROOT"].'/smeetv/func.php');
-
+require_once($_SERVER['DOCUMENT_ROOT']."/smeetv/aquarium/aquarium/filter.php");
 connect2db();
 
 
 $qh="select keyword,counter from keywords order by counter desc limit 0,30";
 $qh=mysql_query($qh);
+$output="";
 for($i=0;$i<mysql_num_rows($qh);$i++){
     $gh=mysql_fetch_array($qh);
-    echo "
-         <span><a href=\"\" style=\"opacity: ".(($gh['counter']/10)+0.25).";font-size:".(($gh['counter']*2)+10)."px\" title=\"{$gh['keyword']}\" id=\"{$gh['keyword']}\">{$gh['keyword']}</a></span>
+
+
+
+    $wd=filter($gh['keyword']);
+
+
+    $output.= "
+         <span><a href=\"\" style=\"opacity: ".(($gh['counter']/10)+0.25).";font-size:".(($gh['counter']*2)+10)."px\" title=\"{$gh['keyword']}\" id=\"{$gh['keyword']}\">$wd[0]</a></span>
     ";
 }
+echo $output;
+
 
 
 
