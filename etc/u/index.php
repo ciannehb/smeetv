aae48cee4259988bae0026d636a2d6eec0e99b10
f1vlad <kulchitski@gmail.com>
2011-06-09 00:46:18 -0400

    Utilize /u/ function to track user being online so we can run more priority twit_fetch

--- etc/u/index.php
+++ etc/u/index.php
@@ -8,31 +8,25 @@ if(!isUserLoggedIn()==1){
     return;
 }
 
-
-
-
 connect2db();
 
 if($_POST['h']!='null'){
-$query="select id,timestamp from twits where uid='{$_POST['i']}' order by id desc limit 0,1;";
-$go=mysql_query($query);
-$get=mysql_fetch_array($go);
-$new=md5($get['id']."".$get['timestamp']);
-$compareto=$_POST['h'];
-
-// echo $_POST['h'] . " " . $compareto;
-
-
-if($new == $compareto)
-    echo "0";
-else
-    echo "1";
-
-}else{
-$query="select id from twits where uid='{$_POST['i']}' order by id desc limit 0,1";
-$go=mysql_query($query);
-echo mysql_num_rows($go);
+    $query="select id,timestamp from twits where uid='{$_POST['i']}' order by id desc limit 0,1;";
+    $go=mysql_query($query);
+    $get=mysql_fetch_array($go);
+    $new=md5($get['id']."".$get['timestamp']);
+    $compareto=$_POST['h'];
+    if($new == $compareto)
+        echo "0";
+    else
+        echo "1";
+    mysql_query("update accounts set last_time='".time()."' where id={$_POST['i']}");
+}/*else{
+    $query="select id from twits where uid='{$_POST['i']}' order by id desc limit 0,1";
+    $go=mysql_query($query);
+    echo mysql_num_rows($go);
 }
+*/
 
 
 return;
