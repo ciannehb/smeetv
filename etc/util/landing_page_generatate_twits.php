4cb5a38344d921dc4e78b512e3b29fb86b221aac
f1vlad <kulchitski@gmail.com>
2011-06-11 20:19:52 -0400

    Add landing page content

---
+++ etc/util/landing_page_generatate_twits.php
@@ -0,0 +1,25 @@
+<?
+require_once($_SERVER["DOCUMENT_ROOT"].'/smeetv/func.php');
+
+connect2db();
+$query="select id,content,link,date,timestamp from twits_dump order by id desc limit 0,10";
+$go=mysql_query($query);
+$output="";
+for($i=0;$i<mysql_num_rows($go);$i++){
+    $get=mysql_fetch_array($go);
+
+    //echo $get[0]."<br><hr>";
+
+
+    $output.=displayTwit($get['id'],$get['content'],$get['link'],$get['date'],$get['timestamp'],0);
+    
+
+}
+
+
+$f = fopen("_landing_content.txt", "w");
+fwrite($f, $output);
+fclose($f);
+
+
+?>
--- login.php
+++ login.php
@@ -10,18 +10,8 @@ if(isUserLoggedIn()==1){
 require_once($_SERVER['DOCUMENT_ROOT'].'/smeetv/header.php');
 drawHeader('Welcome ',$u,'1');
 
-connect2db();
-$query="select id,content,link,date,timestamp from twits_dump order by id desc limit 0,10";
-$go=mysql_query($query);
-for($i=0;$i<mysql_num_rows($go);$i++){
-    $get=mysql_fetch_array($go);
 
-    //echo $get[0]."<br><hr>";
-
-
-    echo displayTwit($get['id'],$get['content'],$get['link'],$get['date'],$get['timestamp'],0);
-
-}
+require_once($_SERVER['DOCUMENT_ROOT'].'/etc/util/_landing_content.txt');
 
 ?>
 
