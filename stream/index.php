3054cee7ebf15865b6f5ca542e156110f8cfbfe5
f1vlad <kulchitski@gmail.com>
2011-06-19 14:22:54 -0400

    Create streams

---
+++ stream/index.php
@@ -0,0 +1,37 @@
+<?
+    include($_SERVER['DOCUMENT_ROOT'].'/smeetv/func.php');
+    session_start();
+
+    if(isUserLoggedIn()==1){
+        $u=true;
+    }
+
+    require_once($_SERVER['DOCUMENT_ROOT'].'/smeetv/header.php');
+    connect2db();
+
+
+    $arr=explode("/",$_SERVER['REQUEST_URI']);
+    $id=advancedClean(3,$arr[count($arr)-1]);
+    $query="select id from accounts where idhash='$id'";
+    $go=mysql_query($query);
+    $get=mysql_fetch_array($go);
+    $query="select * from twits_dump where content REGEXP '#".$id."$|#".$id." ' limit 0,30";
+    $go=mysql_query($query);
+
+    drawHeader('streaming #'.$id,$u);
+
+    echo "<h2>#$id</h2>";
+
+
+
+  
+
+for($i=0;$i<mysql_num_rows($go);$i++){
+    $get=mysql_fetch_array($go);
+    $twusr=explode("/",$get['link']);
+    echo displayTwit($get['id'],$get['content'],$get['link'],$get['date'],$get['timestamp'],0,0);
+}
+?>
+
+
+<?require_once($_SERVER['DOCUMENT_ROOT'].'/smeetv/footer.php');?>
