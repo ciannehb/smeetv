34bbeee10540b2b07558315bc85b898fd4cdefa0
f1vlad <kulchitski@gmail.com>
2011-06-26 02:52:37 -0400

    Modify scraping more, add more sources

--- etc/util/crontab/push_index_dump_normal.sh
+++ etc/util/crontab/push_index_dump_normal.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-curl http://fs.f1vlad.com/smeetv/smeetv.php/index.onequery.php?priority=0 > /dev/null 
-curl http://fs.f1vlad.com/smeetv/smeetv.php/index.onequery.php?priority=1 > /dev/null 
-curl http://fs.f1vlad.com/smeetv/smeetv.php/index.onequery.php?priority=2 > /dev/null
+curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null 
+curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null 
+curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null
+curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null
 
 
 
