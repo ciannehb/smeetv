686d6c72d9aca4ae2a717cc053959f3b1dd8f16d
f1vlad <kulchitski@gmail.com>
2011-07-12 01:47:54 -0400

    Adjust frequency of pushes

--- etc/util/crontab/push_index_dump_normal.sh
+++ etc/util/crontab/push_index_dump_normal.sh
@@ -6,4 +6,16 @@ curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null
 curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null
 
 
+sleep 10; curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null 
+sleep 10; curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null 
+sleep 10; curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null
+sleep 10; curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null
+
+
+
+sleep 35; curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null 
+sleep 40; curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null 
+sleep 48; curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null
+sleep 55; curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null
+
 
