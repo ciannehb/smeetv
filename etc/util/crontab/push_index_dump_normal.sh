16b42d96bb4e7dd345719d8913f085660b5cfdce
f1vlad <kulchitski@gmail.com>
2011-08-01 15:30:32 -0400

    Modify sleep delay

--- etc/util/crontab/push_index_dump_normal.sh
+++ etc/util/crontab/push_index_dump_normal.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
-curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null 
-curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null 
-curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null
-curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null
-curl http://smeetv.com/etc/util/index.onequery.php?priority=4 > /dev/null
+curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null; 
+curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=4 > /dev/null;
 
 
-sleep 10; curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null 
-sleep 10; curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null 
-sleep 10; curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null
-sleep 10; curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null
+sleep 7;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null;
+
+sleep 10;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null;
 
 
-sleep 20; curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null 
-sleep 23; curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null 
-sleep 25; curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null
-sleep 26; curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null
-sleep 26; curl http://smeetv.com/etc/util/index.onequery.php?priority=4 > /dev/null
+sleep 20;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=4 > /dev/null;
 
 
-sleep 35; curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null 
-sleep 40; curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null 
-sleep 48; curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null
-sleep 55; curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null
+sleep 35;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null;
+curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null;
 
 
