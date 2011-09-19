59f79a8869749e8346efef8189fefc2000062b34
f1vlad <kulchitski@gmail.com>
2011-09-18 23:38:00 -0400

    Comment out some pushes temporarily

--- etc/util/crontab/push_index_dump_normal.sh
+++ etc/util/crontab/push_index_dump_normal.sh
@@ -6,26 +6,25 @@ curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null;
 curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null;
 curl http://smeetv.com/etc/util/index.onequery.php?priority=4 > /dev/null;
 
+#sleep 7;
+#curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null;
+#curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null;
+#curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null;
+#curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null;
 
-sleep 7;
-curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null;
-curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null;
-curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null;
-curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null;
-
-sleep 10;
-curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null;
-curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null;
-curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null;
-curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null;
+#sleep 10;
+#curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null;
+#curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null;
+#curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null;
+#curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null;
 
 
-sleep 20;
-curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null;
-curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null;
-curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null;
-curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null;
-curl http://smeetv.com/etc/util/index.onequery.php?priority=4 > /dev/null;
+#sleep 20;
+#curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null;
+#curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null;
+#curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null;
+#curl http://smeetv.com/etc/util/index.onequery.php?priority=3 > /dev/null;
+#curl http://smeetv.com/etc/util/index.onequery.php?priority=4 > /dev/null;
 
 
 sleep 35;
--- etc/util/crontab/push_index_dump_priority.sh
+++ etc/util/crontab/push_index_dump_priority.sh
@@ -3,12 +3,12 @@
 sleep 2;  curl http://smeetv.com/etc/util/index.keyword.php?priority=1 > /dev/null
 sleep 10;  curl http://smeetv.com/etc/util/index.keyword.php?priority=2 > /dev/null
 sleep 20;  curl http://smeetv.com/etc/util/index.keyword.php?priority=3 > /dev/null
-sleep 30;  curl http://smeetv.com/etc/util/index.keyword.php?priority=4 > /dev/null
-sleep 40;  curl http://smeetv.com/etc/util/index.keyword.php?priority=5 > /dev/null
-sleep 50;  curl http://smeetv.com/etc/util/index.keyword.php?priority=6 > /dev/null
-sleep 60;  curl http://smeetv.com/etc/util/index.keyword.php?priority=7 > /dev/null
-sleep 70;  curl http://smeetv.com/etc/util/index.keyword.php?priority=8 > /dev/null
-sleep 80;  curl http://smeetv.com/etc/util/index.keyword.php?priority=9 > /dev/null
-sleep 90;  curl http://smeetv.com/etc/util/index.keyword.php?priority=10 > /dev/null
+#sleep 30;  curl http://smeetv.com/etc/util/index.keyword.php?priority=4 > /dev/null
+#sleep 40;  curl http://smeetv.com/etc/util/index.keyword.php?priority=5 > /dev/null
+#sleep 50;  curl http://smeetv.com/etc/util/index.keyword.php?priority=6 > /dev/null
+#sleep 60;  curl http://smeetv.com/etc/util/index.keyword.php?priority=7 > /dev/null
+#sleep 70;  curl http://smeetv.com/etc/util/index.keyword.php?priority=8 > /dev/null
+#sleep 80;  curl http://smeetv.com/etc/util/index.keyword.php?priority=9 > /dev/null
+#sleep 90;  curl http://smeetv.com/etc/util/index.keyword.php?priority=10 > /dev/null
 
 
