3bd00f13d83a424dd1045cac3d78d00f10df51eb
f1vlad <kulchitski@gmail.com>
2012-03-06 18:16:25 -0500

    Leftovers

--- etc/util/crontab/push_index_dump_normal.sh
+++ etc/util/crontab/push_index_dump_normal.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 curl http://smeetv.com/etc/util/index.onequery.php?priority=0 > /dev/null;
 curl http://smeetv.com/etc/util/index.onequery.php?priority=1 > /dev/null; 
 curl http://smeetv.com/etc/util/index.onequery.php?priority=2 > /dev/null;
